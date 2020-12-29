43959a5460461b8884f9d0465427cb1db0e0fc53
Nayte91 <nayte91@gmail.com>
2020-12-29 15:55:38 +0100

    Relationship between Users and Customer

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -28,6 +28,12 @@ class User
      */
     private $email;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="users")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $customer;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -53,6 +59,18 @@ class User
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): self
+    {
+        $this->customer = $customer;
 
         return $this;
     }
